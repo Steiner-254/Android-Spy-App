7d0fea6ba82461af9200ca7f07790298cbbe3283
steiner-254 <steineralvin3@gmail.com>
2024-06-19 21:10:54 +0300

    update

--- Spy-App/ExampleInstrumentedTest.kt
+++ Spy-App/ExampleInstrumentedTest.kt
@@ -1,1 +1,25 @@
 package me.hawkshaw
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.test
+import org.junit.runner.AndroidJUnit4
+
+import org.junit.Assert.*
+
+/* 
+* Instrumented test, which will execute on an Android device.
+*
+*  Reference --> http://d.android.com/tools/testing OR https://developer.android.com/studio/test
+*/
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test 
+    fun useAppContext() {
+        // Context of the Spy App under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("me.adobot", appContext.packageName)
+    }
+}
