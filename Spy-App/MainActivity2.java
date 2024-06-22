0e8c187950ef0f79d3a0d2136c8934ab55012409
steiner-254 <steineralvin3@gmail.com>
2024-06-22 10:37:04 +0300

    update

---
+++ Spy-App/MainActivity2.java
@@ -0,0 +1,6 @@
+package me.hawkshaw.test;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
