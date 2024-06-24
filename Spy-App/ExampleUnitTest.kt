9be9fbaa70c628c118608e7aff2e47c7f112a5d3
steiner-254 <steineralvin3@gmail.com>
2024-06-24 16:13:31 +0300

    update

---
+++ Spy-App/ExampleUnitTest.kt
@@ -0,0 +1,13 @@
+package me.hawkshaw
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2+2)
+    }
+}
